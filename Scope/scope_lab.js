502a84c6e890c3b9738d6107abdb855074c52528
geravid <eric.garduno@outlook.com>
2026-01-01 15:54:21 -0500

    Extra exercise

--- Scope/scope_lab.js
+++ Scope/scope_lab.js
@@ -15,16 +15,38 @@ function show(){
     let functionLet = "I'm a block-scoped let";
     const functionConst = "I'm a block-scoped const";
 }
-show();
+//show();
+
+{
+    let a = 10;
+    const e = 2.96;
+    var b = true;
+
+    // a = 20;
+    // e = 3.14;
+    // b = false;
+    console.log(a);
+    console.log(e);
+    console.log(b);
+}
+
+
+a = 20;
+e = 3.14;
+b = false;
+
+console.log(a);
+console.log(e);
+console.log(b);
 
 // Global scope
-console.log(globalVar); // Output: "I'm a global variable"
-console.log(globalLet); // Output: "I'm also global, but scoped with let"
-console.log(globalConst); // Output: "I'm a global constant"
+// console.log(globalVar); // Output: "I'm a global variable"
+// console.log(globalLet); // Output: "I'm also global, but scoped with let"
+// console.log(globalConst); // Output: "I'm a global constant"
 
 // console.log(blockVar);
 // console.log(blockLet);
     
-console.log(functionVar); // Throws ReferenceError
-console.log(functionLet); // Throws ReferenceError
-console.log(functionConst); // Throws ReferenceError
+// console.log(functionVar); // Throws ReferenceError
+// console.log(functionLet); // Throws ReferenceError
+// console.log(functionConst); // Throws ReferenceError
